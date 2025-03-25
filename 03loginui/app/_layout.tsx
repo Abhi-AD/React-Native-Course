debeab1557bec268ce6148c12be4e104290964df
Abhi-AD <dangiabhi332@gmail.com>
2025-03-26 00:17:07 +0545

    03 simpleui

---
+++ 03loginui/app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: true }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+      </Stack>
+      <StatusBar hidden />
+      {/* Or: <StatusBar backgroundColor='blue' style='light' /> */}
+    </>
+  );
+}
