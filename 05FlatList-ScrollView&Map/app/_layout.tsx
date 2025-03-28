2c80ed618a8829c0b88990edb2bdca27317ff6a7
Abhi-AD <dangiabhi332@gmail.com>
2025-03-28 22:01:22 +0545

    add the 05flastlistscollview

---
+++ 05FlatList-ScrollView&Map/app/_layout.tsx
@@ -0,0 +1,16 @@
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+
+export default function RootLayout() {
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+      </Stack>
+      {/* <StatusBar hidden /> */}
+      <StatusBar backgroundColor='#ff0000' style='inverted' />
+    </>
+  );
+}
